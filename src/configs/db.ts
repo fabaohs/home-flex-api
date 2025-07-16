9543d34d332eef6e511174de8dc8c2788b731531
Fábio Henrique <fabiohenriquesf8@gmail.com>
2025-07-16 09:07:52 -0300

    chore: just changed few things in ifle sys

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,10 +1,10 @@
 ﻿import { drizzle } from "drizzle-orm/node-postgres";
-import { DATABASE_URL } from "./DATABASE_URL";
 import { Pool } from "pg";
-import * as schemas from "../../shared/entities/index.ts";
+import * as schemas from "../shared/entities/index.ts";
 
 const pool = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString:
+    process.env.DATABASE_URL || "postgres://user:password@localhost:5432/db",
 });
 
 export const db = drizzle(pool, {
--- src/configs/db/DATABASE_URL.ts
+++
@@ -1,3 +0,0 @@
-﻿export const DATABASE_URL =
-  process.env.DATABASE_URL ||
-  "postgres://your-user:your-password@localhost:5432/your-db";
